60563316434cba4136f595b5a15798efde878826
XtremePineapple <95henryX@gmail.com>
2019-06-04 20:36:07 -0700

    stuff

--- jsStuff/main.js
+++ jsStuff/main.js
@@ -2,5 +2,11 @@ import React, {Component} from 'react'
 import {Route} from 'react-router-dom'
 
 export default class Main extends Component {
-    
+    render(){
+        return(
+            <div id="main">
+                <h1>Star Wars-ians</h1>
+            </div>
+        )
+    }
 }
